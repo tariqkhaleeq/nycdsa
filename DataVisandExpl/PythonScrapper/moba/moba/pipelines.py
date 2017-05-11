2a2c66d24b9c5770fb9a888d60056150ebc68263
tariqkhaleeq <tariq.khaleeq@gmail.com>
2017-05-11 13:49:40 +0200

    Added - to complete the list irrespective of the champ having more then one role.

--- DataVisandExpl/PythonScrapper/moba/moba/pipelines.py
+++ DataVisandExpl/PythonScrapper/moba/moba/pipelines.py
@@ -17,19 +17,11 @@ class MobaPipeline(object):
         self.file.close()
 
     def process_item(self, item, spider):
-        if item['pos2']=='':
-            line = str(item['name'][0]) + '\t' + str(item['alias'][0]) + '\t' \
-            + str(item['pos1'][0]) + '\t' + str(item['pickratepos1'][0]) + '\t'\
-            + str(item['damage'][0]) + '\t' + str(item['toughness'][0]) + '\t' \
-            + str(item['cc'][0]) + '\t' + str(item['mobility'][0]) + '\t'\
-            + str(item['utility'][0]) + '\n'
-            self.file.write(line)
-        else:
-            line = str(item['name'][0]) + '\t' + str(item['alias'][0]) + '\t' \
-            + str(item['pos1'][0]) + '\t' + str(item['pickratepos1'][0]) + '\t' + str(item['winrate1'][0]) + '\t' \
-            + str(item['pos2'][0]) + '\t' + str(item['pickratepos2'][0]) + '\t' + str(item['winrate2'][0]) + '\t' \
-            + str(item['damage'][0]) + '\t' + str(item['toughness'][0]) + '\t' \
-            + str(item['cc'][0]) + '\t' + str(item['mobility'][0]) + '\t'\
-            + str(item['utility'][0]) + '\n'
-            self.file.write(line)
+        line = str(item['name'][0]) + '\t' + str(item['alias'][0]) + '\t' \
+        + str(item['pos1'][0]) + '\t' + str(item['pickratepos1'][0]) + '\t' + str(item['winrate1'][0]) + '\t' \
+        + str(item['pos2'][0]) + '\t' + str(item['pickratepos2'][0]) + '\t' + str(item['winrate2'][0]) + '\t' \
+        + str(item['damage'][0]) + '\t' + str(item['toughness'][0]) + '\t' \
+        + str(item['cc'][0]) + '\t' + str(item['mobility'][0]) + '\t'\
+        + str(item['utility'][0]) + '\n'
+        self.file.write(line)
         return item
