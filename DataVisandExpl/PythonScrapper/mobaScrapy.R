9d61ca75b1746050ba83053bf0558da87628ca31
tariqkhaleeq <tariq.khaleeq@gmail.com>
2017-05-12 15:06:38 +0200

    Updated for functions, stats and graphs

--- DataVisandExpl/PythonScrapper/mobaScrapy.R
+++ DataVisandExpl/PythonScrapper/mobaScrapy.R
@@ -115,3 +115,46 @@ ggplot(pwr2, aes(x=pos2,y=pickrate2)) + geom_tile(aes(fill=winrate2))
 ## Top 5 champs in each position and their win rates
 
 
+### calculations for best champ
+
+champ1<-loldata[23,1]
+champ2<-loldata[55,1]
+
+champ1data<-loldata %>% filter(Champ == champ1) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+champ2data<-loldata %>% filter(Champ == champ2) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+
+combo<-rbind(champ1data[,c(1,4:8)],champ2data[,c(1,4:8)])
+
+comboChart<-gvisColumnChart(combo,options=list(legend='bottom',colors="['#004949','#198989','#36A679','#E46B2A','#F48950']"))
+plot(comboChart)
+
+statChamp1<- (sum(champ1data[,4:8])*champ1data$damage^2) * (as.integer(strsplit(champ1data$winrate1,"[%]")[[1]]))/100
+
+whoWins<- function(champ1,champ2){
+  champ1data<-loldata %>% filter(Champ == champ1) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+  champ2data<-loldata %>% filter(Champ == champ2) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+  
+  statChamp1<- (sum(champ1data[,4:8])*champ1data$damage^2) * (as.integer(strsplit(champ1data$winrate1,"[%]")[[1]]))/100
+  statChamp2<- (sum(champ2data[,4:8])*champ2data$damage^2) * (as.integer(strsplit(champ2data$winrate1,"[%]")[[1]]))/100
+  
+  if (statChamp1 > statChamp2){
+    return (paste("The winner is: ",champ1))
+  }else if (statChamp1 == statChamp2){
+    return (paste("Both champs are equal. The winner would depend on farming and champ build"))
+  }else{
+    return (paste("The winner is: ",champ2))
+  }
+  
+}
+
+thewinner<-whoWins(champ1,champ2)
+
+champStats<-function(champ1,champ2){
+  champ1data<-loldata %>% filter(Champ == champ1) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+  champ2data<-loldata %>% filter(Champ == champ2) %>% select(Champ, pickrate1, winrate1, damage, toughness, cc, mobility, utility)
+  
+  combo<-rbind(champ1data[,c(1,4:8)],champ2data[,c(1,4:8)])
+  
+  comboChart<-gvisColumnChart(combo,options=list(legend='bottom',colors="['#004949','#198989','#36A679','#E46B2A','#F48950']"))
+  return (comboChart)
+  }
