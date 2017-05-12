f477f310d31dc3a2dae73c5cb50709e9d4015af5
tariqkhaleeq <tariq.khaleeq@gmail.com>
2017-05-12 17:39:12 +0200

    Added libraries in global and added txt file and shinyapp.io folder

--- DataVisandExpl/PythonScrapper/moba-app/global.R
+++ DataVisandExpl/PythonScrapper/moba-app/global.R
@@ -1,7 +1,10 @@
 # global.R for moba
 library(shinydashboard)
+library(dplyr)
+library(ggplot2)
+library(googleVis)
 
-loldata<-read.delim("~/nycdsa/DataVisandExpl/PythonScrapper/moba/moba/spiders/LOL_moba_champs.txt",header = FALSE, stringsAsFactors = FALSE)
+loldata<-read.delim("LOL_moba_champs.txt",header = FALSE, stringsAsFactors = FALSE)
 # add colum names
 colnames(loldata)<-c("Champ","Alias","pos1","pickrate1","winrate1","pos2","pickrate2","winrate2","damage","toughness","cc","mobility","utility")
 
