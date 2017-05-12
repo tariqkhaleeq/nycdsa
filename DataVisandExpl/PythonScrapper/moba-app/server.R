21622b781bb418f53a9c96e239573ae611af6d2d
tariqkhaleeq <tariq.khaleeq@gmail.com>
2017-05-12 17:00:32 +0200

    Fully functional Team matchup and result

--- DataVisandExpl/PythonScrapper/moba-app/server.R
+++ DataVisandExpl/PythonScrapper/moba-app/server.R
@@ -4,7 +4,7 @@ source("./global.R")
 
 shinyServer(function(input,output){
   
-  output$answer<-renderPrint({
+  output$answer<-renderText({
     if (input$your_champ=="" | input$opp_champ==""){
       "Select a Champion"
     }else{
@@ -15,4 +15,17 @@ shinyServer(function(input,output){
   output$champGraph<-renderGvis({
     champStats(input$your_champ,input$opp_champ)
   })
+  
+  output$teamAnswer<-renderText({
+    if (input$your_champ1=="" |input$your_champ2=="" |input$your_champ3=="" |input$your_champ4=="" |input$your_champ5=="" | input$opp_champ1==""| input$opp_champ2==""| input$opp_champ3==""| input$opp_champ4==""| input$opp_champ5==""){
+      "Please select all Champions"
+    }else{
+      whowinsTeam(input$your_champ1,input$your_champ2,input$your_champ3,input$your_champ4,input$your_champ5,input$opp_champ1,input$opp_champ2,input$opp_champ3,input$opp_champ4,input$opp_champ5)
+    }
+  })
+  
+  output$teamchampGraph<-renderGvis({
+    teamStats(input$your_champ1,input$your_champ2,input$your_champ3,input$your_champ4,input$your_champ5,input$opp_champ1,input$opp_champ2,input$opp_champ3,input$opp_champ4,input$opp_champ5)
+  })
+  
 })
